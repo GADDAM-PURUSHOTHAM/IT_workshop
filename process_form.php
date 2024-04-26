8238dcea4b6f5e94d7e441fa4b749c9262cdab64
GADDAM-PURUSHOTHAM <165474408+GADDAM-PURUSHOTHAM@users.noreply.github.com>
2024-04-26 11:30:43 +0000

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,11 +1,11 @@
 <?php
-// Check if the form is submitted
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-// Retrieve form data
+
 $name = $_POST["name"];
 $email = $_POST["email"];
-$message = $_POST["message"];// Process the data (e.g., store in database, send email, etc.)
-// For simplicity, just echoing here
+$message = $_POST["message"];
+
 echo "Name: $name <br>";
 echo "Email: $email <br>";
 echo "Message: $message <br>";
